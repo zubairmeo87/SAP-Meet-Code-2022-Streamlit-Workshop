0965c7dec3af5177aeea9a6e19eaa52a28dace3f
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2022-10-14 19:43:30 +0200

    Update Slide1.py

--- content/Slide1.py
+++ content/Slide1.py
@@ -1,9 +1,9 @@
 st.header("Tun'Up Munich e.V. presentation")
-image = Image.open('content/images/slide 1.png')
+image = Image.open('content/images/slide 1.PNG')
 st.image(image)
-image = Image.open('content/images/slide2.png')
+image = Image.open('content/images/slide2.PNG')
 st.image(image)
-image = Image.open('content/images/slide 3.png')
+image = Image.open('content/images/slide 3.PNG')
 st.image(image)
 st.text("""Tun'Up Munich e.V. is a social and humanitarian non-profit organization dedicated to improving the educational environment in Tunisia and empower the youthful generation.
 We, members of Tun'Up Munich e.V., believe that education is the key to growth and success. We believe investing in it is investing in our future. 
@@ -13,8 +13,8 @@ Our vision is, everyone should have access to it and our goal is ensuring opport
 - Endorsement of social commitment in education through oriented and collaborative projects.
 - Being an active part of the educational system's development in Tunisia.""")
 st.subheader("Current Projects")
-image = Image.open('content/images/projects.png')
+image = Image.open('content/images/projects.PNG')
 st.image(image)
 st.subheader("Project Ex: Primary school Sidi Said in Siliana-Bargou")
-image = Image.open('content/images/slides 4.png')
+image = Image.open('content/images/slides 4.PNG')
 st.image(image)
