bc29500d7380a6033a70c328d4a5aac93f52ca9f
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2022-10-22 10:42:41 +0200

    Update Slide2.py

--- content/Slide2.py
+++ content/Slide2.py
@@ -1,5 +1,17 @@
 st.header("Getting Started with streamlit")
-
+st.subheader("Quick introduction:")
+st.text("Streamlit, an app framework built for ML engineers and its main purpose is to turn Python Scripts into Beautiful ML Tools.")
+st.subheader("Quick history:")
+st.text("The framework was designed and implemented by two Google Engineers in order to simplify the process of building and maintaining UI applications (ex: ML dashboards) where original process (figure below) seemed unmaintainable and unefficient.")
+image = Image.open('content/images/slide 2_first_image.png')
+st.image(image)
+st.subheader("General simple idea from streamlit:")
+st.text("What if we could make building tools as easy as writing Python scripts?")
+st.subheader("Read more:")
+st.markdown("""
+-	Article from streamlit founders: https://towardsdatascience.com/coding-ml-tools-like-you-code-ml-models-ddba3357eace 
+- Interesting article from DataCamp: https://www.datacamp.com/tutorial/streamlit 
+""")
 st.markdown("""
 - Streamlit is an open source app framework in Python language. It helps us create web apps for data science and machine learning in a short time. 
 - Founded in 2018 by ex Google engineers(Adrien Treuille, Amanda Kelly and Thiago Teixeira
